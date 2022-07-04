48243a4760518d1e99dd9b2f354cda13eb7322f1
sabin149 <dangalsabin149@gmail.com>
2022-07-04 15:26:48 +0545

    booking model and controller updated

--- model/bookingModel.js
+++ model/bookingModel.js
@@ -1,60 +1,71 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 const bookingSchema = new mongoose.Schema({
-    room:{
-        type: mongoose.Types.ObjectId,ref:"room"
+    room: {
+        type: mongoose.Types.ObjectId, ref: "room"
     },
-    user:{ type: mongoose.Types.ObjectId, ref: 'user' },
-    hotel:{
-        type: mongoose.Types.ObjectId,ref:"hotel"
+    user: { type: mongoose.Types.ObjectId, ref: 'user' },
+    hotel: {
+        type: mongoose.Types.ObjectId, ref: "hotel"
     },
-    start_date:{
+    start_date: {
+        type: Date,
+        required: true,
+    },
+    end_date: {
         type: Date,
         required: true,
 
     },
-    end_date:{
-        type: Date,
-        required: true,
-
-    },
-    total_amount:{
+    total_amount: {
         type: Number,
         required: true,
     },
-    name:{
+    name: {
         type: String,
         required: true,
     },
-    email:{
+    email: {
         type: String,
         required: true,
     },
-    phone:{
+    phone: {
         type: String,
         required: true,
         minlength: 10,
         maxlength: 10,
     },
-    address:{
+    address: {
         type: String,
         required: true,
     },
-    request:{
+    request: {
         type: Array
     },
-    tc:{
+    tc: {
         type: Boolean,
         required: true,
     },
-    payment_id:{
+    payment_id: {
         type: String,
         required: true,
     },
-    payment_type:{
+    payment_type: {
         type: String,
         required: true,
     },
-
-})
+    children: {
+        type: Number
+    },
+    adults: {
+        type: Number
+    },
+    rooms:{
+        type: Number
+    }
+},
+    {
+        timestamps: true,
+    }
+)
 module.exports = mongoose.model("booking", bookingSchema)
 
